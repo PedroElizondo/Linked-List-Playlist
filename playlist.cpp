4770c06a8b9294db86d414d0911717b95c17af11
PedroElizondo <143659554+PedroElizondo@users.noreply.github.com>
2023-10-16 17:16:30 -0500

    Update playlist.cpp

--- playlist.cpp
+++ playlist.cpp
@@ -1,1 +1,29 @@
+#include "songs.cpp"
 
+class playlist{
+  // private variables that you think belong in a playlist
+  string currentsongPlaylist;
+  Song * head;
+  Song * tail;
+
+  public:
+  // Methods that you'll be implementing
+  void addSong();
+  void removeSong();
+  void displayPlaylist();
+];
+
+playlist::addSong()
+{
+
+}
+
+playlist::removeSong()
+{
+
+}
+
+playlist::displayPlaylist()
+{
+
+}
